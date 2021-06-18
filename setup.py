00ffc680888047137a675533db326295e9855a5e
Troy Williams <troy.williams@bluebill.net>
2021-06-18 12:27:55 -0400

    Added setup.py stub.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+# -----------
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Troy Williams
+
+# uuid:
+# author: Troy Williams
+# email:  troy.williams@bluebill.net
+# date:
+# -----------
+
+"""
+setup.py stub to allow `pip install .` to work and use setup.cfg
+
+https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+
+from setuptools import setup
+
+setup()
